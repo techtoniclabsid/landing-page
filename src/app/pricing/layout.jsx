dad77a4fdf8dea9afdff6c6911f23a8a0e7e414c
alkuinvito <alkuinvito@tutanota.com>
2025-02-02 02:01:48 +0700

    feat: add more description in metadata

--- src/app/pricing/layout.jsx
+++ src/app/pricing/layout.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const metadata = {
   title: "Pricing - Techtoniclabs",
   description:
-    "Techtoniclabs offers you the flexible pricing model, for your business at any scale.",
+    "At Techtoniclabs, we offer a flexible pricing model designed to meet the needs of businesses at any scale. Whether you're a startup, a growing company, or an established enterprise, our tailored pricing plans ensure you get the right solutions without breaking the bank. We understand that every business is unique, which is why our pricing is customizable, transparent, and scalable to fit your specific requirements. From basic packages to premium options, we provide cost-effective solutions that deliver exceptional value and support your growth. Explore our pricing plans today and discover how we can help you achieve your goals with the perfect balance of quality and affordability. Let us build something great together!",
 };
 
 const Layout = ({ children }) => {
